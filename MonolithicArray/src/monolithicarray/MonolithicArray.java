7458afb999e570c794e37f5736867c355f2e4a68
siddhiparekh11 <parekhsiddhi11@gmail.com>
2018-12-28 21:51:38 -0800

    commit

--- MonolithicArray/src/monolithicarray/MonolithicArray.java
+++ MonolithicArray/src/monolithicarray/MonolithicArray.java
@@ -10,38 +10,37 @@ public class MonolithicArray {
   // int[] arr ={6,5,4,3,0};
   
   //int[] arr ={1};
-  int[] arr = {1,2,2,-1,0,32};
+  int[] arr = {1,1,0};
     System.out.println(mono(arr));
     
   }
   
   public static boolean mono(int[] A){
-    if(A.length==0)
-            return false;
-        if(A.length==1)
-            return true;
-        
-        int flag=-1;
+    boolean flag1,flag2;
+        flag1=flag2=false;
         
         for(int i=0;i<A.length-1;i++){
-            if(A[i]<=A[i+1] && flag==0){
-                flag=0;
-                continue;               
-            }else if(A[i]>=A[i+1] && flag==1){
-                flag=1;
+            if(A[i]==A[i+1])
                 continue;
-            }else if(A[i]<=A[i+1] && flag==-1){
-                flag=0;
-                continue;                
-            }else if(A[i]>=A[i+1] && flag==-1){
-                flag=1;
-                continue;                
-            }else{
-                return false;
-            }
+            if(A[i]<A[i+1])
+                flag1=true;
+            if(A[i]>A[i+1])
+                flag2=true;      
+            
         }
         
+        if(flag1 && flag2)
+                return false;
+        
+        if(flag1 && !flag2)
+            return true;
+        
+        if(flag2 && !flag1)
+           return true;
+        
         return true;
+        
+       
 
   }
   
